60fd2917c8b79d50c7a81084d457bee76f762fb5
Brian Sigafoos <sigafoos@gmail.com>
2023-02-05 14:00:55 -0500

    Add GPT from scratch (#6)
    * Add GPT from scratch
    
    * Add bigram.py
    
    * Add bigram v2 from video lecture
    
    * Add timing
    
    * Add download_data.py
    
    * Add v2b, following the video
    
    * Add gpt.py
    
    * Fix lint
    
    * Organize file
    
    * Fix lint

---
+++ gpt_from_scratch/download_data.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
+TINY_SHAKESPEARE_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+path_data = Path(".data")
+path_data.mkdir(exist_ok=True)
+path_file = path_data / "tinyshakespeare.txt"
+
+if not path_file.exists():
+    urlretrieve(TINY_SHAKESPEARE_URL, path_file)
+
+print("Data file now available at", path_file)
+print("First 100 characters: \n")
+print(path_file.read_text()[:100])
